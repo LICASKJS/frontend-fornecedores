8f5947b6eb99300d3a68b30517aec1608eba44c6
LICASKJS <licasmateusdasilva25@gmail.com>
2025-09-29 15:12:35 -0300

    Atualiza frontend com últimas mudanças (cadastro, recuperação de senha, etc)

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -277,7 +277,7 @@ const handleSubmit = async (e) => {
   }
 
   try {
-    const response = await fetch('https://meu-backend-i134.onrender.com/api/cadastro', {
+    const response = await fetch('https://backend-fornecedores.onrender.com/api/cadastro', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
